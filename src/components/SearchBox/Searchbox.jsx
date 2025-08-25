e107d58d454ff5fcc74ea414a929e99852037300
furkangoit <celikfurkan45@hotmail.com>
2025-08-25 17:22:01 +0300

    edited

--- src/components/SearchBox/Searchbox.jsx
+++ src/components/SearchBox/Searchbox.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./SearchBox.module.css";
 
-export default function SearchBox({ value, onChange }) {
+export default function SearchBox({ search, handleSearch }) {
   return (
     <div className={styles.searchbox}>
       <label>
         Search:
         <input
           type="text"
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
+          value={search}
+          onChange={handleSearch}
           className={styles.input}
           placeholder="Type a name..."
         />

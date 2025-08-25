8e94ef24b3dfa228da2b36e9a315f7a4810383cc
furkangoit <celikfurkan45@hotmail.com>
2025-08-25 21:14:19 +0300

    Fix SearchBox import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
 import { useState, useEffect } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
-import SearchBox from "./components/SearchBox";
+import SearchBox from "./components/SearchBox";  // ← Burada değişiklik!
 import data from "./data.json";
 
 function App() {
   const [search, setSearch] = useState("");
   const [contacts, setContacts] = useState(data);
+
   useEffect(() => {
     const localContacts = localStorage.getItem("contacts");
     if (localContacts) setContacts(JSON.parse(localContacts));
   }, []);
+
   useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts)), [contacts];
-  });
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   function handleSearch(e) {
     setSearch(e.target.value.toLowerCase());
   }
+
   function addContact(newContact) {
     setContacts((c) => [...c, newContact]);
   }
+
   function deleteContact(id) {
     setContacts((contacts) => contacts.filter((c) => c.id !== id));
   }
+
   const filteredContacts = contacts.filter((c) =>
     c.name.toLowerCase().includes(search)
   );
+
   return (
     <div>
       <h1>Phonebook</h1>
@@ -35,4 +42,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
